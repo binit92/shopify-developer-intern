db20d99d7725f59cff33591116e7ed4d29a205ca
Binit Kumar <kumar.binit1992@gmail.com>
2022-01-16 11:59:46 -0500

    Adding certificates files generated using openssl

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Shopify-Developer-Intern
+
+
+# TODO ITEMS:
++ Don't save all the images in same folder but different folder based on total size of folder
+https://stackoverflow.com/questions/348363/what-is-the-best-place-for-storing-uploaded-images-sql-database-or-disk-file-sy
+
++ How to identify duplicate images while uploading and not allow it to upload ? 
+
++ 
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from wsgiref.util import request_uri
 from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
@@ -26,16 +27,20 @@ def upload_file():
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
+
+        files = request.files.getlist("file")
+        print("files: ", files)
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
+        for file in files:
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                #return redirect(url_for('download_file', name=filename))
+        return "Images have been uploaded" , 200
     return
 
 @app.route('/uploads/<name>')
@@ -48,5 +53,10 @@ def hello():
 
 if __name__ == "__main__":
     # use of on-the-fly certificates for HTTPS
-    app.run(ssl_context='adhoc', debug = True)
+    # commenting it for now as hosting in localhost throws warning
+    # ref: https://blog.miguelgrinberg.com/post/running-your-flask-application-over-https
+
+    #app.run(ssl_context='adhoc', debug = True)
+    #app.run(debug = True)
+    app.run(ssl_context=('cert.pem', 'key.pem'))
         
